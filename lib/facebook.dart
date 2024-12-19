56bc668825057df80d7ff77714d0417426970ea1
asifnawaz077 <kgul3532@gmail.com>
2024-12-19 16:12:35 +0500

    init

---
+++ lib/facebook.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+
+class Facebook extends StatelessWidget {
+  const Facebook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'facebook',
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[800]),
+            ),
+            actions: [
+              SizedBox(width: 20),
+              Icon(Icons.add_circle_rounded),
+              SizedBox(width: 20),
+              Icon(Icons.search),
+              SizedBox(width: 20),
+              Icon(Icons.message),
+              SizedBox(width: 20),
+            ],
+          ),
+          body: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('images/Sharjeel.jpeg'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                    height: 35,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.black12),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 15, top: 7),
+                      child: Text("What's on your mind?"),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 15.0),
+                    child: Icon(
+                      Icons.photo_album_outlined,
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+              Divider(
+                thickness: 3,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Container(
+                        height: 180,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('images/Sharjeel.jpeg'),
+                              fit: BoxFit.cover),
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 14.0, bottom: 5),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CircleAvatar(
+                                backgroundImage: AssetImage('images/Sharjeel.jpeg'),
+                              ),
+                              Text(
+                                'Ahmad',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 11),
+                              ),
+                            ],
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                      height: 180,
+                      width: 130,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('images/Sharjeel.jpeg'),
+                            fit: BoxFit.cover),
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 14.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage('images/Sharjeel.jpeg'),
+                            ),
+                            Text(
+                              'Sharjeel',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 11),
+                            ),
+                          ],
+                        ),
+                      )),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                      height: 180,
+                      width: 130,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('images/Sharjeel.jpeg'),
+                            fit: BoxFit.cover),
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 14.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage('images/Sharjeel.jpeg'),
+                            ),
+                            Text(
+                              'Ahmad',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 11),
+                            ),
+                          ],
+                        ),
+                      )),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                      height: 180,
+                      width: 130,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('images/Sharjeel.jpeg'),
+                            fit: BoxFit.cover),
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 14.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage('images/Sharjeel.jpeg'),
+                            ),
+                            Text(
+                              'Sharjeel',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 11),
+                            ),
+                          ],
+                        ),
+                      )),
+                ]),
+              ),
+              Divider(
+                thickness: 3,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('images/Muizz.png'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Abdul Muiz',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 56.0),
+                            child: Row(
+                              children: [
+                                Text('1h'),
+                                SizedBox(
+                                  width: 7,
+                                ),
+                                Icon(Icons.public)
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 325.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Winter"),
+                    SizedBox(
+                      height: 5,
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 350,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/Muizz.png'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.favorite_outline),
+                    Text('101k'),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(Icons.chat_bubble_outline,),
+                    Text('33.1k')
+                    
+                  ],
+
+                ),
+              ),
+            ],
+            ),
+          ),
+        )
+    );
+  }
+}
